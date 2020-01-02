760ad1479fbdf26dd8ccb12bc25abeea005c0047
ppyrpw <peterwoods+git@fastmail.fm>
2020-01-02 16:09:01 -0600

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,14 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  shareit() {
+    window.alert('Spamlicious!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
 
 
